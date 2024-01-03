a41cfe4834105ab5c6b2ea5be8d3480b171e3f07
KALORSOR <alorsorgodsway@gmail.com>
2024-01-03 01:31:41 -0800

    Header

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -23,4 +23,12 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+
 #endif /* LISTS_H */
