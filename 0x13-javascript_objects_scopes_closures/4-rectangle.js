aeb2dcd748c4246b784753edc4a3462c51cb5df6
KALORSOR <alorsorgodsway@gmail.com>
2024-04-09 06:21:36 -0700

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,28 @@
 #!/usr/bin/node
-//Check the parameters provided
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+    if ((w = parseInt(w)) > 0 &&
+            (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let i = 0; i < this.height; i++) {
-      let myVar = '';
-      let y = 0;
-      while (y < this.width) {
-        myVar += 'X';
-        y++;
-      }
-      console.log(myVar);
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
-    let temp = 0;
-    temp = this.width;
+    const rotatedValues = this.width;
     this.width = this.height;
-    this.height = temp;
+    this.height = rotatedValues;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-}
-module.exports = Rectangle;
+};
